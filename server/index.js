84122c7c1301a89233da6659a8e8c2d21949a9ac
Dorjee <rishav345@gmail.com>
2023-01-23 19:56:36 -0500

    back--end updates, new dependencies

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3001
+require("dotenv").config();
+const OpenAI = require('openai-api');
+const OPENAI_API_KEY = process.env.API_KEY;
+
+const openai = new OpenAI(OPENAI_API_KEY);
+
+
+
+(async () => {
+    const gptResponse = await openai.complete({
+        engine: 'davinci',
+        prompt: 'this is a test',
+        maxTokens: 5,
+        temperature: 0.9,
+        topP: 1,
+        presencePenalty: 0,
+        frequencyPenalty: 0,
+        bestOf: 1,
+        n: 1,
+        stream: false,
+        stop: ['\n', "testing"]
+    });
+
+    console.log(gptResponse.data);
+})();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
